73bcac4475dee1655f098cf3fedb167a2d9cfc4b
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-03-06 14:24:10 +0300

    updated readme

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import categoriesSlice from "./Slices/categoriesSlice";
-import productsSlice from "./Slices/productsSlice";
+import categoriesSlice, { getCategories } from "./Slices/categoriesSlice";
+import productsSlice, { getProducts } from "./Slices/productsSlice";
 import { apiSlice } from "./Slices/api/apiSlice";
 import userSlice from "./Slices/userSlice";
+
+
 
 export const store = configureStore({
     reducer: {
@@ -11,8 +13,17 @@ export const store = configureStore({
         user: userSlice,
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
-    middleware: (getMiddleware) => getMiddleware().concat(apiSlice.middleware),
+    // middleware: (getMiddleware) => getMiddleware().concat(apiSlice.middleware),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+const setupListeners = () => {
+    store.dispatch(getProducts());
+    store.dispatch(getCategories());
+};
+  
+setupListeners(); 
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
